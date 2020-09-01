5e656fee7ec97990ff318dd285aef91a0aa9e394
Arthur Descartes Rego <57276470+Regaul@users.noreply.github.com>
2020-09-01 21:17:45 +0100

    Create rhcooc.py
    I'll add more later

---
+++ rhcooc.py
@@ -0,0 +1,14 @@
+moment_dict = {grumm_littlegirls:'https://cdn.discordapp.com/attachments/696052039771357186/750429199851847900/image0.png,', Brady_elismom:'https://cdn.discordapp.com/attachments/696052039771357186/750428924563030116/image0.png',
+naemt_fortnite:'https://cdn.discordapp.com/attachments/696052039771357186/748014691917365338/image0.png', calvin_calvin:'https://cdn.discordapp.com/attachments/696052039771357186/745895293882597397/calvin.PNG',
+fluffer_flufferlist:'https://cdn.discordapp.com/attachments/696052039771357186/743980247137124406/unknown.png', grumm_pedo:'https://cdn.discordapp.com/attachments/696052039771357186/744940000310722560/Capture_2020-08-03-15-12-58.png',
+eli_nudes:'https://cdn.discordapp.com/attachments/697587669051637760/743875608353832990/image0.jpg', lucy-lucky_twinkiesex:'https://cdn.discordapp.com/attachments/696052039771357186/742876226372501544/image0.png',
+del_toychicasex:'https://cdn.discordapp.com/attachments/696052039771357186/742391973209702420/image0.png', danna-lucy_suicide:'https://cdn.discordapp.com/attachments/696052039771357186/741794203821932544/image0.png',
+grumm-fluffer_elidating:'https://cdn.discordapp.com/attachments/728809490329239634/749418642973917234/image1.png', del-pinkuyo_fuckoff:'https://cdn.discordapp.com/attachments/728809490329239634/744849730294644886/image0.png',
+fluffer_based:'https://cdn.discordapp.com/attachments/728809490329239634/735886754472656958/image0.png', grumm-merms_beatsaber:'https://cdn.discordapp.com/attachments/728809490329239634/733125236815560755/image0.png',
+regaul_funnymeme:'https://cdn.discordapp.com/attachments/728809490329239634/733071888368009346/unknown.png', regaul_skullsquad:'https://cdn.discordapp.com/attachments/728809490329239634/732804116644102274/Screen_Shot_2020-07-15_at_12.39.17_AM.png',
+chainsword_deaththreats:'https://cdn.discordapp.com/attachments/728809490329239634/731762633308635207/isdeaththreats.png', fluffer-eli_changed:'https://cdn.discordapp.com/attachments/728809490329239634/730561162600513576/image0.png',
+ceilingfan-monkey-regaul_roleplay:'https://cdn.discordapp.com/attachments/728809490329239634/730147148599132180/Screen_Shot_2020-07-07_at_4.43.28_PM.png',
+peter_pc:'https://cdn.discordapp.com/attachments/728809490329239634/729825585182015498/rate_my_pc.png', eli_zoophile:'https://cdn.discordapp.com/attachments/728809490329239634/729812214013362339/easy.png'
+rai-eli_adminentry:'https://cdn.discordapp.com/attachments/728809490329239634/729181857304019015/image0.png'}
+
+
